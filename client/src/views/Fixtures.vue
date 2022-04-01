d4c19cd3bcf2af401812edba34316154d678890b
Nisarg Dave <extrastuff58@yahoo.com>
2022-04-01 20:03:29 +0800

    Removed components to start again

--- client/src/views/Fixtures.vue
+++ client/src/views/Fixtures.vue
@@ -1,40 +1,11 @@
 <template>
-  <div style="height: 100%" class="d-flex flex-column">
-    <div class="d-flex flex-row-reverse">
-      <RoundSelect @selectedRound="selectRound" />
-    </div>
-    <div class="d-flex justify-center">
-      <h1 class="white--text">Fixtures</h1>
-    </div>
-    <div class="flex-grow-1">
-      <FixturesTable :selectedRound="selectedRound" :key="componentKey" />
-    </div>
-  </div>
+  <div></div>
 </template>
 
 <script>
-  import RoundSelect from "../components/fixtures/RoundSelect.vue";
-  export default {
-    name: "Fixtures",
-    data() {
-      return {
-        selectedRound: "",
-        componentKey: 0,
-      };
-    },
-    components: {
-      FixturesTable: () => ({
-        component: import("../components/fixtures/FixturesTable.vue"),
-      }),
-      RoundSelect,
-    },
-    methods: {
-      selectRound(round) {
-        this.selectedRound = round;
-        this.componentKey++;
-      },
-    },
-  };
+export default {
+  name: "Fixtures",
+};
 </script>
 
 <style lang="scss" scoped></style>
